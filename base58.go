0bc4575133fe4867ae6d327820f1773d024e2bb0
huayun321 <huayun321@gmail.com>
2021-03-27 22:27:47 +0800

    add base 58 decode

--- base58.go
+++ base58.go
@@ -1,6 +1,9 @@
 package main
 
-import "math/big"
+import (
+	"bytes"
+	"math/big"
+)
 
 var b58Alphabet = []byte("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
 
@@ -29,4 +32,28 @@ func Base58Encode(input []byte) []byte {
 	}
 
 	return result
+}
+
+// Base58Decode decodes Base58-encoded data
+func Base58Decode(input []byte) []byte {
+	result := big.NewInt(0)
+	zeroBytes := 0
+
+	for b := range input {
+		if b == 0x00 {
+			zeroBytes++
+		}
+	}
+
+	payload := input[zeroBytes:]
+	for _, b := range payload {
+		charIndex := bytes.IndexByte(b58Alphabet, b)
+		result.Mul(result, big.NewInt(58))
+		result.Add(result, big.NewInt(int64(charIndex)))
+	}
+
+	decoded := result.Bytes()
+	decoded = append(bytes.Repeat([]byte{byte(0x00)}, zeroBytes), decoded...)
+
+	return decoded
 }
